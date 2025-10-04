bca96c8a7eb52a63bcd1f3bab987e79a0894a51f
Klein237 <kleinfy51@gmail.com>
2025-10-04 11:17:22 +0200

    feat: description robot

---
+++ my_pkg/launch/display.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os, xacro
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('my_pkg')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    robot_desc = xacro.process_file(xacro_file).toxml()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        # Node(
+        #     package='joint_state_publisher_gui',
+        #     executable='joint_state_publisher_gui',
+        #     name='joint_state_publisher_gui'
+        # ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
